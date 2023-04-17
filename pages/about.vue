63772ed1f2921717d4e4a2d83c9eeaa0cbb57a8a
Nariman <abonent26pg@mail.ru>
2023-04-17 19:42:48 +0300

    init

---
+++ pages/about.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt App</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident sunt
+      consectetur alias quos repellendus aperiam sed, neque reprehenderit. Odio,
+      dignissimos.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident sunt
+      consectetur alias quos repellendus aperiam sed, neque reprehenderit. Odio,
+      dignissimos.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Products details for {{ id }}</p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore,
+      accusantium sapiente. Qui animi nam ut consequuntur quasi praesentium
+      nobis iusto!
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
